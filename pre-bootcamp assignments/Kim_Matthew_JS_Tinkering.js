991c3f7904bf1b364f03d13ac4bac91b96fbc442
Matthew Kim <mattkim83@gmail.com>
2022-02-07 19:37:21 -0800

    indent initial commit

---
+++ pre-bootcamp assignments/Kim_Matthew_JS_Tinkering.js
@@ -0,0 +1,9 @@
+var childHeight = 62;
+ function displayIfChildisAbleToRideTheRollerCoaster () {
+    if (childHeight > 52) {
+        console.log ('Get on that ride, Kiddo!');
+    } else {
+        console.log ('Sorry Kiddo. Maybe next year');
+    }
+};
+displayIfChildisAbleToRideTheRollerCoaster()
