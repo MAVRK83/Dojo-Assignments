901036b1e787bbcceb248040431aa326eed3b019
MAVRK83 <97990974+MAVRK83@users.noreply.github.com>
2022-02-07 19:41:40 -0800

    Delete pre-bootcamp assignments directory

--- pre-bootcamp assignments/Kim_Matthew_JS_Tinkering.js
+++
@@ -1,9 +0,0 @@
-var childHeight = 62;
- function displayIfChildisAbleToRideTheRollerCoaster () {
-    if (childHeight > 52) {
-        console.log ('Get on that ride, Kiddo!');
-    } else {
-        console.log ('Sorry Kiddo. Maybe next year');
-    }
-};
-displayIfChildisAbleToRideTheRollerCoaster()
