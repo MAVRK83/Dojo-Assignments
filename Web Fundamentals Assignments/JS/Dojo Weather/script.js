5f4d0d0ed2fc18176567d19abfb31c8b8695102f
Matthew Kim <mattkim83@gmail.com>
2022-03-01 22:18:55 -0800

    commit

--- Web Fundamentals Assignments/JS/Dojo Weather/script.js
+++ Web Fundamentals Assignments/JS/Dojo Weather/script.js
@@ -1,32 +1,37 @@
-// onsole.log("page loading...");
+onsole.log("page loading...");
 
-// var cookieDiv = document.querySelector(".cookie-policy");
+var cookieDiv = document.querySelector(".cookie-policy");
 
-// function loading() {
-//     alert("Loading weather report...")
-// }
+function loading() {
+    alert("Loading weather report...")
+}
 
-// function accept() {
-//     cookieDiv.remove();
-// }
+function accept() {
+    cookieDiv.remove();
+}
 
-// function c2f(temp) {
-//     return Math.round(9 / 5 * temp + 32);
-// }
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
+}
 
-// function f2c(temp) {
-//     return Math.round(5 / 9 * (temp - 32));
-// }
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
 
-// function convert(element) {
-//     console.log(element.value);
-//     for(var i=1; i<9; i++) {
-//         var tempSpan = document.querySelector("#temp" + i);
-//         var tempVal = parseInt(tempSpan.innerText);
-//         if(element.value == "°C") {
-//             tempSpan.innerText = f2c(tempVal);
-//         } else {
-//             tempSpan.innerText = c2f(tempVal);
-//         }
-//     }
-// }
+function convert(element) {
+    console.log(element.value);
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = f2c(tempVal);
+        } else {
+            tempSpan.innerText = c2f(tempVal);
+        }
+    }
+}
+
+function closeThis() {
+    var acceptBtn = document.getElementById ("footer")
+    acceptBtn.style.visibility = "hidden";
+}
