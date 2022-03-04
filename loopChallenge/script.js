e4d6038d85ad16d2dd7d6f718527fb88b6b71ff0
Matthew Kim <mattkim83@gmail.com>
2022-03-03 22:09:26 -0800

    loop challenge

---
+++ loopChallenge/script.js
@@ -0,0 +1,21 @@
+// for (var i=1; i<20; i++) {
+//     if (i % 2 ==1){
+//         console.log (i)
+//     } 
+// }
+
+// for (var i=100; i>0; i--) {
+//     if (i % 3 ==0) {
+//         console.log(i)
+//     }
+// }
+
+// for (var i=5.5; i> -3.5; i) {
+//     if(i -= 1.5)
+//     console.log(i)
+// }
+
+for (var i=1; i<=100; i++) {
+    sum = (i + (i+1))
+    console.log (sum) 
+}
