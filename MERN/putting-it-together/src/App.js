cad39e08c9bf884b7b5eeae23233b29f121d158a
Matthew Kim <mattkim83@gmail.com>
2022-03-15 22:25:55 -0700

    commit

---
+++ MERN/putting-it-together/src/App.js
@@ -0,0 +1,71 @@
+import logo from "./logo.svg"
+import "./App.css";
+import PersonCard from "./components/PersonCard";
+
+
+const peopleArr = [
+  {
+    firstName: "John",
+    lastName: "Smith",
+    age: 30,
+    hairColor: "black",
+  },
+  {
+    firstName: "Matt",
+    lastName: "Kim",
+    age: 38,
+    hairColor: "black",
+  },
+  {
+    firstName: "Eunice",
+    lastName: "Kim",
+    age: 38,
+    hairColor: "blonde",
+  },
+  {
+    firstName: "Roy",
+    lastName: "Kim",
+    age: 48,
+    hairColor: "black",
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      {peopleArr.map((personObj, index) => (
+        <PersonCard
+        key={index}
+        firstName={personObj.firstName}
+        lastName={personObj.lastName}
+        age={personObj.age}
+        hairColor={personObj.hairColor}
+        />
+      ))}
+      {/* <PersonCard
+        firstName= {peopleArr[0].firstName}
+        lastName={peopleArr[0].lastName}
+        age={peopleArr[0].hairColor}
+        />
+      <PersonCard
+        firstName= {peopleArr[1].firstName}
+        lastName={peopleArr[1].lastName}
+        age={peopleArr[1].hairColor}
+        />
+      <PersonCard
+        firstName= {peopleArr[2].firstName}
+        lastName={peopleArr[2].lastName}
+        age={peopleArr[2].hairColor}
+        />
+      <PersonCard
+        firstName= {peopleArr[3].firstName}
+        lastName={peopleArr[3].lastName}
+        age={peopleArr[3].hairColor}
+        /> */}
+      
+      
+    </div>
+  );
+  }
+
+export default App;
